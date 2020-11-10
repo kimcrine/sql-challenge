830a6e6f65344b54995e28182dc7dcbabfb9e11d
Kim Crine <69007385+kimcrine@users.noreply.github.com>
2020-11-10 16:45:32 -0500

    completed part 6

--- EmployeeSQL/DataEngineeringQueries.sql
+++ EmployeeSQL/DataEngineeringQueries.sql
@@ -31,3 +31,11 @@ FROM employees
 WHERE
 first_name = 'Hercules'
 AND last_name LIKE 'B%'
+
+-- 6. List all employees in the Sales department, including their 
+-- employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+INNER JOIN dept_emp ON dept_emp.emp_no = employees.emp_no
+INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE dept_name = 'Sales'
