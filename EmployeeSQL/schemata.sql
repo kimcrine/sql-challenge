20b4fb587b64f2bd2db322585e553333d3972492
Kim Crine <69007385+kimcrine@users.noreply.github.com>
2020-11-10 15:01:32 -0500

    adding schemata file. created tables and imported csvs

---
+++ EmployeeSQL/schemata.sql
@@ -0,0 +1,36 @@
+CREATE TABLE departments (
+	dept_no VARCHAR(50),
+	dept_name VARCHAR(50)
+);
+
+CREATE TABLE dept_emp (
+	emp_no INT,
+	dept_no VARCHAR(50)
+);
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(50),
+	emp_no INT
+);
+
+CREATE TABLE employees (
+	emp_no INT,
+	emp_title_id VARCHAR(50),
+	birth_date DATE,
+	first_name VARCHAR(255),
+	last_name VARCHAR(255),
+	sex VARCHAR,
+	hire_date DATE,
+	PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE salaries (
+	emp_no INT,
+	salary INT
+);
+
+CREATE TABLE titles (
+	title_id VARCHAR(50),
+	title VARCHAR(50),
+	PRIMARY KEY(title_id)
+);
