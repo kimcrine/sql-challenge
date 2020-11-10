a61e30f200223939593276d7e1689171b53591eb
Kim Crine <69007385+kimcrine@users.noreply.github.com>
2020-11-10 15:51:47 -0500

    adding data engineering sql file to store queries. completed part one

---
+++ EmployeeSQL/DataEngineeringQueries.sql
@@ -0,0 +1,7 @@
+-- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+
+-- Perform an INNER JOIN on the two tables
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+INNER JOIN salaries ON
+employees.emp_no=salaries.emp_no;
