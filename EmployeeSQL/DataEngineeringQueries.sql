0441081ea3bb8d861e102b7edc2352f3301e6909
Kim Crine <69007385+kimcrine@users.noreply.github.com>
2020-11-10 16:15:41 -0500

    completed part 3

--- EmployeeSQL/DataEngineeringQueries.sql
+++ EmployeeSQL/DataEngineeringQueries.sql
@@ -10,3 +10,10 @@ employees.emp_no=salaries.emp_no;
 SELECT employees.first_name, employees.last_name, employees.hire_date
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+-- 3. List the manager of each department with the following information: 
+-- department number, department name, the manager's employee number, last name, first name.
+SELECT dept_manager.dept_no, departments.dept_name, employees.emp_no, employees.last_name, employees.first_name  
+FROM employees
+INNER JOIN dept_manager ON dept_manager.emp_no = employees.emp_no
+INNER JOIN departments ON dept_manager.dept_no = departments.dept_no;
